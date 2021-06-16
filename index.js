88e3ac258bf82edabe474619a1fd6be15986cefe
Erick Castille <ecastille924@gmail.com>
2021-06-16 18:28:16 -0400

    commit tree 199ee4815c06472244c39075527292199d5f86c3

--- index.js
+++ index.js
@@ -29,3 +29,19 @@ function moveDodgerRight() {
       moveDodgerRight();
     }
   });  
+
+
+
+
+// let dodger = document.getElementById("dodger");
+
+// dodger.style.backgroundColor = "pink";
+
+// document.addEventListener("keydown", function(e){
+//   if(e.key === "ArrowLeft") {
+//     let leftNumbers = dodger.style.left.replace("px", "");
+//     let left = parseInt(leftNumbers, 10);
+
+//     dodger.style.left = `${left -1}px`;
+//   }
+// })
